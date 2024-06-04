127a7aa117d2cf41e6ff420fe6684183dacd2694
CLARA VITORIA ALVES DIAS <0000121684544xsp@aluno.educacao.sp.gov.br>
2024-06-04 13:54:40 -0300

    status

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("Flamengo o melhor")
+function calcular(){
+    var fPeso = parseFloat(document.getElementById("peso"))
+    var fAltura = parseFloat(document.getElementById("altura"))
+    var fIMC = fPeso/(fAltura*fAltura)
+}
